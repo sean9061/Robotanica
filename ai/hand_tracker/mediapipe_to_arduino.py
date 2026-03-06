68c94efb257e3206b5cb2472a56b848270ae9ec6
kakkason <kakipi7010@ozzio.jp>
2026-03-07 02:14:52 +0900

    adjust mediapipe for arduino

--- ai/hand_tracker/mediapipe_to_arduino.py
+++ ai/hand_tracker/mediapipe_to_arduino.py
@@ -14,8 +14,8 @@ BG_COLOR = (0, 100, 100)           # (R, G, B) 黒
 cap = cv2.VideoCapture(0)          # デフォルトカメラ（0番）を開く
 
 # カメラ解像度を「要求」する（通らないこともある）
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 mp_hands = mp.solutions.hands      # 手検出モジュールへのショートカット
 mp_draw = mp.solutions.drawing_utils  # 骨格描画ユーティリティ
@@ -60,43 +60,53 @@ def main():
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # BGR → RGB 変換
 
             results = hands.process(rgb)  # 手の骨格検出を実行
+            isHandTracking = bool(results.multi_hand_landmarks) # 検出されたかどうか
 
-            canvas = background.copy()    # 背景をコピー（毎フレームまっさらにする）
+            # canvas = background.copy()    # 背景をコピー（毎フレームまっさらにする）
+            canvas = frame;
+            if isHandTracking:  # 手が検出された場合
+                for i, hand_landmarks in enumerate(results.multi_hand_landmarks):   #検出された各手について
 
-            if results.multi_hand_landmarks:  # 手が検出された場合
-                for hand_landmarks in results.multi_hand_landmarks:  # 検出された各手について
-                    mp_draw.draw_landmarks(
-                        canvas,              # 描画先（カメラ画像ではなく背景）
-                        hand_landmarks,      # 手のランドマーク情報
-                        mp_hands.HAND_CONNECTIONS  # 指の接続線情報
-                    )
-                    thumb = hand_landmarks.landmark[4]   #親指先端座標
-                    #root = hand_landmarks.landmark[5]   #人差し指根本座標
-                    index = hand_landmarks.landmark[8]  #人差し指先端座標
-                    pos1 = np.array([int(thumb.x * w), int(thumb.y * h)])
-                    pos2 = np.array([int(index.x * w), int(index.y * h)])
-                    vec = pos2 - pos1
-                    center = (pos1 + pos2)/2
-                    center = center.astype('int')
-                    vrt = np.array([0,-w])
-                    angle = calc_angle(vec, vrt)
-                    radius = int(np.linalg.norm(vec)/2)
-                    
-                    servo = servo_control(radius, angle, ntrl, 1)
-                    
-                    packet = bytes([0xFF, servo[0], servo[1], servo[2]]) #頭4桁はArduinoでの到着判定用
-                    #if ser.out_waiting:    #out_waitingを書くとArduino側でSerial.available()がfalseになる
-                    ser.write(packet)     #Arduinoへシリアル通信で送信
-                    
-                    #if ser.in_waiting:
-                    # read = ser.readline().decode().strip()
-                    
-                    cv2.putText(canvas, str(servo), (100, 150),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  #サーボ制御量を表示
-                    # cv2.putText(canvas, str(packet), (100, 200),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  #Arduinoへの送信を表示
-                    # cv2.putText(canvas, str(read), (100, 300),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  #Arduinoからの受信を表示
-                    cv2.line(canvas, pos1, pos2, color=(0, 255, 0))                 #直線を描画
-                    cv2.circle(canvas, center, radius, color=(0, 255, 0))    #円を描画
-            
+                    label = results.multi_handedness[i].classification[0].label
+                    if label == "Right":    #右手なら
+                        mp_draw.draw_landmarks(
+                            canvas,              # 描画先（カメラ画像ではなく背景）
+                            hand_landmarks,      # 手のランドマーク情報
+                            mp_hands.HAND_CONNECTIONS  # 指の接続線情報
+                        )
+                        thumb = hand_landmarks.landmark[4]   #親指先端座標
+                        #root = hand_landmarks.landmark[5]   #人差し指根本座標
+                        index = hand_landmarks.landmark[8]  #人差し指先端座標
+                        pos1 = np.array([int(thumb.x * w), int(thumb.y * h)])
+                        pos2 = np.array([int(index.x * w), int(index.y * h)])
+                        vec = pos2 - pos1
+                        center = (pos1 + pos2)/2
+                        center = center.astype('int')
+                        vrt = np.array([0,-w])
+                        angle = int(calc_angle(vec, vrt))
+                        angle_8bit = int(np.clip(angle, 0, 255))
+                        radius = int(np.linalg.norm(vec)/2)
+                        radius_8bit = int(np.clip(radius, 0, 255))
+                        packet = bytes([0xFF, isHandTracking, radius_8bit, angle_8bit]) #頭4桁はArduinoでの到着判定用
+                        
+                        #if ser.out_waiting:    #out_waitingを書くとArduino側でSerial.available()がfalseになる
+                        ser.write(packet)     #Arduinoへシリアル通信で送信
+                        
+                        #if ser.in_waiting:
+                        # read = ser.readline().decode().strip()    #受信
+                        
+                        cv2.putText(canvas, str(angle), pos2, cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0)) 
+                        # cv2.putText(canvas, str(packet), (100, 200),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  
+                        # cv2.putText(canvas, str(), (100, 300),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  #Arduinoからの受信を表示
+                        cv2.line(canvas, pos1, pos2, color=(0, 255, 0))                 #直線を描画
+                        cv2.circle(canvas, center, radius, color=(0, 255, 0))    #円を描画
+                    else:
+                        cv2.putText(canvas, "Use Right Hand", (100, 200),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))
+            else:
+                packet = bytes([0xFF, isHandTracking, 0x00, 0x00]) #頭4桁はArduinoでの到着判定用
+                ser.write(packet)
+                cv2.putText(canvas, "No Hands Found", (100, 200),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0, 255, 0))  
+                
             cv2.imshow("MediaPipe Hands", canvas)  # 画面表示
 
             if cv2.waitKey(1) & 0xFF == 27:  # ESCキーで終了
@@ -111,10 +121,10 @@ def calc_angle(vec1, vec2):
     norm1 = np.linalg.norm(vec1)
     norm2 = np.linalg.norm(vec2)
     cross = np.cross(vec1, vec2)
-    theta = np.arccos(inner/(norm1*norm2))
+    theta = np.degrees(np.arccos(inner/(norm1*norm2)))
     if cross < 0:
         theta *= -1
-    return theta
+    return theta + 180
 
 #極座標からサーボ制御量
 def servo_control(r, theta, neutral, gain):
