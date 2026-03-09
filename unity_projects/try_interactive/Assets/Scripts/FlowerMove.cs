c0b97d100c5449104370ee7e2be087c93f6de3d8
kakkason <kakipi7010@ozzio.jp>
2026-03-10 05:47:40 +0900

    動的にサイズ変えようとして失敗した

--- unity_projects/try_interactive/Assets/Scripts/FlowerMove.cs
+++ unity_projects/try_interactive/Assets/Scripts/FlowerMove.cs
@@ -1,34 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FlowerMove : MonoBehaviour
 {
     public UDPReceiver_ udp;
-    public float moveScale;
-    public float depthScale;
+
+    public float moveScale = 1f;
+    public float depthScale = 1f;
+
+    public Vector3 moveRange = new Vector3(1,1,1);
+
     private Vector3 startPos;
-    // Start is called before the first frame update
+    float x,y,w,h,z;
+
+
     void Start()
     {
         startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float x = udp.latestData.x; //0~1
-        float y = udp.latestData.y; //0~1
-        float w = udp.latestData.w; //0~1
-        float h = udp.latestData.h; //0~1
-        float conf = udp.latestData.conf; //0~1
+        x = udp.latestData.x;
+        y = udp.latestData.y;
+        w = udp.latestData.w;
+        h = udp.latestData.h;
 
-        float z = Mathf.Sqrt(w*w + h*h);
+        z = Mathf.Sqrt(w*w + h*h);
 
         x = -2*x + 1;
         y = -2*y + 1;
-        z = z*depthScale;
+        z = z * depthScale;
 
-        transform.position = new Vector3(x, y, z) * moveScale + startPos;
+        Vector3 targetPos = new Vector3(x, y, z);
+        targetPos = Vector3.Scale(targetPos, moveRange) + startPos;
+        transform.position = targetPos;
+
     }
+
+    
 }
